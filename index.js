905c0c0d202980d0e825f767e36042854d2c7f30
Cleo Aguiar <cleotaisas@gmail.com>
2019-02-28 11:51:10 -0300

    feat: dispay a random quote

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-const newQuoteButton  = document.querySelector('#new-quote');
-newQuoteButton.addEventListener('click', getQuote);
 
-function getQuote(){
+function getQuote() {
 	fetch(endpoint)
 	.then(function(response){
 		return response.json();
 	})
 	.then(function(data){
-		console.log(data.message);
+		displayQuote(data.message);
 	})
 	.catch(function(){
 		console.log("Error");
 	});
 }
+
+function displayQuote(quote) {
+	const quoteText = document.querySelector('#text');
+	quoteText.textContent = quote;
+}
+
+const newQuoteButton  = document.querySelector('#new-quote');
+newQuoteButton.addEventListener('click', getQuote);
