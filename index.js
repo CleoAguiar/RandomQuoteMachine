ba15f8641902121ffea51ab4a4d5bc88e19b7521
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-01 09:21:02 -0300

    feat: twitter button. Userstory #10

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+// const endpoint = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
 
 function getQuote() {
 	fetch(endpoint)
@@ -7,6 +8,7 @@ function getQuote() {
 	})
 	.then(function(data){
 		displayQuote(data.message);
+		// console.log(data);
 	})
 	.catch(function(){
 		console.log("Error");
@@ -16,7 +18,12 @@ function getQuote() {
 function displayQuote(quote) {
 	const quoteText = document.querySelector('#text');
 	quoteText.textContent = quote;
+
+	const tweetButton = document.querySelector("#tweet-quote");
+	tweetButton.setAttribute('href', `https://twitter.com/intent/tweet?text=${quote}`);
 }
 
 const newQuoteButton  = document.querySelector('#new-quote');
 newQuoteButton.addEventListener('click', getQuote);
+
+getQuote();
