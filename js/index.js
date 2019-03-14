acdc0eddcf1dccd6693d94473c659b1449e47d32
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-14 11:36:04 -0300

    feat: implement tweet-quote

--- js/index.js
+++ js/index.js
@@ -1,49 +1,19 @@
-// const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 const endpoint = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
 
-function getQuote(){
+function displayQuote(){
 	$.getJSON(endpoint, function(json){
-			const quote = json.quoteText;
-			const author = json.quoteAuthor;
+			var quote = json.quoteText;
+			var author = json.quoteAuthor;
 			$("#text").text(quote);
 			$("#author").text(author);
+			$('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${quote} - by ${author}`);
 		});
 }
 
 $(document).ready(function(){
-	getQuote();
+	displayQuote();
+
 	$("#new-quote").on("click", function(){
-		getQuote();
+		displayQuote();
 	});
 });
-
-
-// function getQuote() {
-// 	fetch(endpoint)
-// 	.then(function(response){
-// 		return response.json();
-// 	})
-// 	.then(function(data){
-// 		displayQuote(data.message);
-// 		// console.log(data);
-// 	})
-// 	.catch(function(){
-// 		console.log("Error");
-// 	});
-// }
-
-// function displayQuote(quote) {
-// 	const quoteText = document.querySelector('#text');
-// 	quoteText.textContent = quote;
-
-// 	const authorText = document.querySelector('#author');
-// 	authorText.textContent =  'Donald Trump';
-
-// 	const tweetButton = document.querySelector('#tweet-quote');
-// 	tweetButton.setAttribute('href', `https://twitter.com/intent/tweet?text=${quote}`);
-// }
-
-// const newQuoteButton  = document.querySelector('#new-quote');
-// newQuoteButton.addEventListener('click', getQuote);
-
-// getQuote();
