1caa5a2b7fb907d0e97c3390957be095d55cf95d
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-27 08:33:32 -0300

    feat: add new-quote click function

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,17 @@
 const endpoint = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
 
 (function($){
-	function displayQuote(){
-		var quote = 'json.quoteText';
-		var author = 'json.quoteAuthor';
-		if (author.length == 0){
-			author = 'Unknown';
-		}
-
+	function displayQuote(quote, author = 'Unknown'){
 		$('#text').text(quote);
 		$('#author').text(author);
 		$('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${quote} - by ${author}`);
 	}
 
 	$(document).ready(function(){
-		displayQuote();
+		displayQuote('json.quoteText', 'json.quoteAuthor');
+
+		$('#new-quote').click(function(){
+			displayQuote('quoteText');
+		});
 	});
 })(jQuery);
