e8e72387cb80cf841a389b25d7c0dffc3ab7816d
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-27 08:11:57 -0300

    feat: new js file

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+const endpoint = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
+
+(function($){
+	$(document).ready(function(){
+		$('#text').text('Hello World!');
+		$('#author').text('Me');
+	});
+})(jQuery);
