968766d2f4e11236cb305a18971da31513138c35
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-27 08:18:43 -0300

    feat: add displayQuote function

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,19 @@
 const endpoint = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
 
 (function($){
+	function displayQuote(){
+		var quote = 'json.quoteText';
+		var author = 'json.quoteAuthor';
+		if (author.length == 0){
+			author = 'Unknown';
+		}
+
+		$('#text').text(quote);
+		$('#author').text(author);
+		$('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${quote} - by ${author}`);
+	}
+
 	$(document).ready(function(){
-		$('#text').text('Hello World!');
-		$('#author').text('Me');
+		displayQuote();
 	});
 })(jQuery);
