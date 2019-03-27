af9e94335eddcc0f6132ae1b6bb743ff95dcfeee
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-27 09:28:40 -0300

    refactor: displayQuote

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,19 @@
 const endpoint = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
 
 (function($){
-	function displayQuote(quote, author = 'Unknown'){
-		$('#text').text(quote);
-		$('#author').text(author);
-		$('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${quote} - by ${author}`);
+	function displayQuote(){
+		$.getJSON(endpoint, function(json){
+			$('#text').text(json.quoteText);
+			$('#author').text(json.quoteAuthor.length == 0 ? 'Unknown' : json.quoteAuthor);
+			$('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${quote} - by ${author}`);
+		});
 	}
 
 	$(document).ready(function(){
-		$.getJSON(endpoint, function(json){
-			displayQuote(json.quoteText, json.quoteAuthor);
-		});
+		displayQuote();
 
 		$('#new-quote').click(function(){
-			displayQuote('quoteText');
+			displayQuote();
 		});
 	});
 })(jQuery);
